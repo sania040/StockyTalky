8a8a756ce438b5ead8e6eaa0723f514a13367636
syedsyab <syabblogger@gmail.com>
2025-04-25 16:21:51 +0500

    Refactor database connection and API data fetching logic; add environment variable support for database credentials.

--- src/db/crypto_db.py
+++ src/db/crypto_db.py
@@ -1,29 +1,31 @@
 import psycopg2
+from .get_connection import get_db_connection
 
-conn = psycopg2.connect(
-    dbname="cryptodb",
-    user="postgres",
-    password="admin",
-    host="localhost",
-    port="5432"
-)
 
-cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS crypto_prices (
-    id SERIAL PRIMARY KEY,
-    symbol VARCHAR(10),
-    price_usd NUMERIC,
-    market_cap_usd NUMERIC,
-    volume_24h_usd NUMERIC,
-    percent_change_24h NUMERIC,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-""")
-
-conn.commit()
-cursor.close()
-conn.close()
-
-print("Table created successfully.")
+def createTable():
+    conn = get_db_connection()
+    if conn is None:
+        print("Failed to connect to the database.")
+        return
+    else :
+        print("Connected to the database.")
+        cursor = conn.cursor()
+    
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS crypto_prices (
+            id SERIAL PRIMARY KEY,
+            symbol VARCHAR(10),
+            price_usd NUMERIC,
+            market_cap_usd NUMERIC,
+            volume_24h_usd NUMERIC,  -- This is correct
+            percent_change_24h NUMERIC,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+    
+        conn.commit()
+        cursor.close()
+        conn.close()
+    
+        print("Table created successfully.")
